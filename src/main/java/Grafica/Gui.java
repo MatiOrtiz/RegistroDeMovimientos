f117c73949b8da67193b8761108bd8f16f4410e5
MatiOrtiz <matiasnortiz.dev@gmail.com>
2023-12-02 17:45:30 -0300

    Panel de ingreso creado

--- src/main/java/Grafica/Gui.java
+++ src/main/java/Grafica/Gui.java
@@ -1,4 +1,13 @@
 package Grafica;
 
+import javax.swing.*;
+
 public class Gui {
+    private JPanel panel1;
+    private JTextField usuarioField;
+    private JPasswordField passwordField;
+    private JLabel lblUsuario;
+    private JLabel lblClave;
+    private JPanel panelIngreso;
+    private JButton btnIngresar;
 }
